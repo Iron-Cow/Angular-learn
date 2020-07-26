5cdd4ef04656b1454ab0edb5055393bb280102ff
Iron-Cow <uangeji@gmail.com>
2020-07-26 16:01:31 +0300

    ngIf, ngSwitch, ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  backgroundToggle = false;
+  toggle: any = false;
+  arr = [1, 1, 2, 3, 5, 8];
+
+  // @ts-ignore
+  objects = [
+    {title: 'Post1', author: "Iurii", comments: [
+        {name: 'Max', text: 'lorem 1'},
+        {name: 'Max', text: 'lorem 2'},
+        {name: 'Max', text: 'lorem 3'}
+      ]},
+     {title: 'Post2', author: "Petia", comments: [
+        {name: 'Max', text: 'lorem 1'},
+        {name: 'Max', text: 'lorem 2'},
+        {name: 'Max', text: 'lorem 3'}
+      ]}
+  ];
 }
