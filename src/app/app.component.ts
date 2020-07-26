001f610dcad861581fa8e0514c2f28146dc0f2af
Iron-Cow <uangeji@gmail.com>
2020-07-26 21:08:00 +0300

    access to HTML elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  now: Date = new Date();
+  posts: Post[] = [
+    {title: 'Angular component', text: 'Still learning component...', id: 1},
+    {title: 'Next blok', text: 'This text about directives... 11235813', id: 1}
+  ];
+
+  updatePosts(post: Post): void{
+    this.posts.unshift(post);
+    console.log("Post", post);
+  }
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,19 @@
-import {Component} from "@angular/core";
+import {Component, Input, OnInit} from '@angular/core';
+import {Post} from "../app.component";
 
 @Component({
-  selector: "app-post",
+  selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+
+  // @Input("myPost") post: Post;
+  @Input() post: Post;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
