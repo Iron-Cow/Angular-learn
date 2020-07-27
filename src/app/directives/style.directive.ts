2d06795a6a40b515021c53e19da5852e2be81907
Iron-Cow <uangeji@gmail.com>
2020-07-27 14:11:30 +0300

    @HostBinding for style

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from "@angular/core";
 
 // @ts-ignore
 @Directive({
@@ -18,21 +18,25 @@ export class StyleDirective {
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue');
   }
 
+  @HostBinding('style.color') elColor = null;
+
   @HostListener('click', ['$event.target']) onClick(event: Event): void {
     console.log(event);
   }
 
   @HostListener('mouseenter') onEnter(): void {
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', this.color);
-    this.renderer.setStyle(this.elRef.nativeElement, 'fontWeight', this.dStyles.fontWeight);
-    this.renderer.setStyle(this.elRef.nativeElement, 'border', this.dStyles.border);
-    this.renderer.setStyle(this.elRef.nativeElement, 'borderRadius', this.dStyles.borderRadius);
+    this.elColor = this.color;
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', this.color);
+    // this.renderer.setStyle(this.elRef.nativeElement, 'fontWeight', this.dStyles.fontWeight);
+    // this.renderer.setStyle(this.elRef.nativeElement, 'border', this.dStyles.border);
+    // this.renderer.setStyle(this.elRef.nativeElement, 'borderRadius', this.dStyles.borderRadius);
   }
 
   @HostListener('mouseleave') onLeave(): void {
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', null);
-    this.renderer.setStyle(this.elRef.nativeElement, 'fontWeight', null);
-    this.renderer.setStyle(this.elRef.nativeElement, 'border', null);
-    this.renderer.setStyle(this.elRef.nativeElement, 'borderRadius', null);
+    this.elColor = null;
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', null);
+    // this.renderer.setStyle(this.elRef.nativeElement, 'fontWeight', null);
+    // this.renderer.setStyle(this.elRef.nativeElement, 'border', null);
+    // this.renderer.setStyle(this.elRef.nativeElement, 'borderRadius', null);
   }
 }
