4a85d48afbdf1a556328f7f710c04f3bc8925897
Iron-Cow <uangeji@gmail.com>
2020-07-30 02:17:01 +0300

    Http parameters in angular

--- src/app/todos.services.ts
+++ src/app/todos.services.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Observable, throwError} from "rxjs";
 import {catchError, delay} from "rxjs/operators";
 
@@ -30,7 +30,14 @@ export class TodosService {
   }
 
   fetchTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+    let params = new HttpParams();
+    params = params.append('_limit', '4');
+    params = params.append('custom', 'anyany');
+
+
+    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos', {
+      params
+    })
       .pipe(
         delay(500),
         catchError(error => {
