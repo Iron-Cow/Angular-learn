33b9ef888a324cbaa9bbffe88e48b2e3b1cc2a6e
Iron-Cow <uangeji@gmail.com>
2020-07-30 00:54:30 +0300

    http client post requests handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,19 +16,42 @@ export class AppComponent implements OnInit{
 
   todos: Todo[] = [];
 
+  todoTitle = '';
+
   constructor(
     private http: HttpClient
   ){}
 
   ngOnInit(): void {
-    // RxJS
-    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
-      .subscribe( todos => {
-        console.log('Response:', todos);
-        this.todos = todos;
-        }
-      );
+    this.fetchTotos();
   }
 
 
+  addTodo(): void{
+    if (!this.todoTitle.trim()){
+      return;
+    }
+
+    const newTodo: Todo = {
+      title: this.todoTitle,
+      completed: false
+    };
+
+    this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', newTodo)
+      .subscribe(todo => {
+        console.log('todo', todo);
+        this.todos.push(todo);
+        this.todoTitle = '';
+      });
+  }
+
+  fetchTotos(): void {
+    // RxJS
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
+      .subscribe( todos => {
+          // console.log('Response:', todos);
+          this.todos = todos;
+        }
+      );
+  }
 }
