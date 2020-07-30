cc7eace2e374d1254bd1c44a0131fdf1066e6026
Iron-Cow <uangeji@gmail.com>
2020-07-31 00:13:25 +0300

    dynamic routing and parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import {Routes, RouterModule} from "@angular/router";
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {PostsComponent} from "./posts/posts.component";
+import {PostComponent} from "./post/post.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'posts', component: PostsComponent},
+  {path: 'posts/:id', component: PostComponent},
 
 ];
 
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,29 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {Post, PostsService} from "../posts.service";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit{
+  post: Post;
 
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostsService,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      console.log('Params', params);
+      this.post = this.postService.getById(+params.id);
+    });
+  }
+
+  loadPost(): void {
+    this.router.navigate(['/posts', 44]);
+  }
 }
