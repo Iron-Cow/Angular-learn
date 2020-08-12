7ddbd89a04aede33d8685aab5dfd21d229ebdd18
Iron-Cow <uangeji@gmail.com>
2020-08-12 16:53:10 +0300

    integration tests (tepmlate/attributes)

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,45 +1,36 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing'
 import {CounterComponent} from "./counter.component";
-import {FormBuilder} from "@angular/forms";
+import {By} from "@angular/platform-browser";
 
 describe('CounterComponent', () => {
   let component: CounterComponent
+  let fixture: ComponentFixture<CounterComponent>
 
   beforeEach(() => {
-    component = new CounterComponent(new FormBuilder())
+    TestBed.configureTestingModule({
+      declarations: [ CounterComponent ]
+    })
+
+    fixture = TestBed.createComponent(CounterComponent)
+    component = fixture.componentInstance
+    // fixture.debugElement
+    // fixture.nativeElement
   })
 
-  // beforeAll, afterEach, afterAll
-
-  it('should increment counter by 1', () => {
-    component.increment()
-    expect(component.counter).toBe(1)
+  it('should be created', () => {
+    expect(component).toBeDefined()
   })
 
-  it('should decrement counter by 1', () => {
-    component.decrement()
-    expect(component.counter).toBe(-1)
-  })
+  it('should render counter property', () => {
+    let num = 42
+    component.counter = num
 
-  it('should increment value by event emitter', () => {
-    let result = null
-    component.counterEmitter.subscribe(v => result = v)
+    fixture.detectChanges()
 
-    component.increment()
+    let de = fixture.debugElement.query(By.css('.counter'))
+    let el: HTMLElement = de.nativeElement
 
-    expect(result).toBe(1)
-  })
-
-  it('should create form with 2 controls', () => {
-    expect(component.form.contains('login')).toBeTruthy()
-    expect(component.form.contains('email')).toBeTruthy()
-  })
-
-  it('should mark login as invalid if empty value', () => {
-    const control = component.form.get('login')
-
-    control.setValue('')
-
-    expect(control.valid).toBeFalsy()
+    expect(el.textContent).toContain(num.toString())
   })
 
 })
