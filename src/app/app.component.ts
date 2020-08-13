8b444460d7ed87a776871d5733b0892011495bb2
Iron-Cow <uangeji@gmail.com>
2020-08-14 00:44:02 +0300

    animation events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -78,4 +78,11 @@ export class AppComponent {
   animate() {
     this.boxState = this.boxState === 'end' ? 'start' : 'end';
   }
+
+  animationStarted($event: AnimationEvent) {
+    console.log('animation started', $event);
+  }
+  animationDone($event: AnimationEvent) {
+    console.log('animation done', $event);
+  }
 }
