96974673eaedee26f65f24f3ce31b85f02e6f436
Iron-Cow <uangeji@gmail.com>
2020-08-13 16:51:56 +0300

    animations complicated transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {trigger, state, style} from "@angular/animations";
+import {trigger, state, style, transition, animate} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -7,11 +7,30 @@ import {trigger, state, style} from "@angular/animations";
   styleUrls: ['./app.component.css'],
   animations: [
     trigger('box', [
-      state('start', style({ background: 'blue' })),
+      state('start', style({
+        background: 'blue',
+        borderRadius: '50%'
+
+      })),
       state('end', style({
         background: 'red',
-        transform: 'scale(1.2)'
-      }))
+        transform: 'scale(1.2)',
+        borderRadius: '0'
+      })),
+      state('special', style({
+        background: 'orange',
+        transform: 'scale(0.5)',
+        borderRadius: '50%'
+      })),
+      transition('start => end', animate(450)),
+      transition('end => start', animate('800ms ease-in-out')),
+      transition('* <=> special', [
+        style({background: 'green'}),
+        animate('1s', style({
+          background: 'pink'
+        })),
+        animate(750),
+      ]),
     ])
   ]
 })
